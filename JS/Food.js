e863b7481ebd81817e70057d59ebddece68907b1
Shaurya443 <81462790+Shaurya443@users.noreply.github.com>
2022-01-15 11:09:25 +0300

    Update Food.js

--- JS/Food.js
+++ JS/Food.js
@@ -1,47 +1,38 @@
-class Food{
- constructor(x,y,width,height){
-var options={
-    isStatic:0.9,
-    friction:0.05
-}
-this.body =Bodies.rectangle(x,y,width,options)
-this.width = width
-this.height = height
-World.add(world,this.body)
-this.image =loadImage("Images/Milk.png")
+class Food {
+  constructor(){
+  this.foodStock=0;
+  this.image=loadImage('images/Milk.png');
+  }
+
+ updateFoodStock(foodStock){
+  this.foodStock=foodStock;
  }
-display(){
-    var x = 80,y =100
 
-    imageMode(CENTER)
-    image(this.image,720,220,70,70)
+ deductFood(){
+   if(this.foodStock>0){
+    this.foodStock=this.foodStock-1;
+   }
+  }
 
+  getFoodStock(){
+    return this.foodStock;
+  }
+
+  display(){
+    var x=80,y=100;
+    
+    imageMode(CENTER);
+ 
+    
     if(this.foodStock!=0){
-        for(var i=0;i<this.foodStock;i++){
-            if(i%10==0){
-                x=80;
-                y=y+50
-            }
-            image(this.image,x,y,50,50)
-            x=x+30
+      for(var i=0;i<this.foodStock;i++){
+        if(i%10==0){
+          x=80;
+          y=y+50;
         }
-
+        image(this.image,x,y,50,50);
+        x=x+30;
+      }
     }
-}
-getFoodStock(){
-    var foodStock = database.ref('foodStock')
-    foodStock.on("value",function(data){
-    foodStock = data.val()
-    })
-  
-
-
-}
-updateFoodStock(foodStock){
-    database.ref('/').update({
-        foodStock:foodStock
-    })
-
-}
-
+  }
 }
